165334b8683ded7d1e988f59712e0de2bd4e43e9
Javier <javivasv@hotmail.com>
2021-10-13 18:33:37 -0400

    Home layout

--- src/templates/views/Home.vue
+++ src/templates/views/Home.vue
@@ -1,5 +1,72 @@
 <template>
-  <h1>HOME</h1>
+  <v-row class="content">
+    <v-col cols="8" id="home-info">
+      <v-card id="welcome-card">
+        <v-row justify="center">
+          <h1>Welcome to the Pokemon Nuzlocke DataBase!</h1>
+        </v-row>
+        <v-card-text>
+          Here you can keep track of all of your pokemon nuzlocke playthroughs
+        </v-card-text>
+      </v-card>
+      <v-card id="videos-card">
+        <h1>VIDEOS</h1>
+      </v-card>
+    </v-col>
+    <v-col cols="4" id="rules">
+      <v-card class="info-card">
+        <v-card-subtitle class="card-text"
+          >Nuzlocke Basic Rules</v-card-subtitle
+        >
+        <v-card-text>
+          <ul>
+            <li>
+              <p class="card-text">
+                Any Pokémon that faints is considered dead, and must be released
+                or put in the Pokémon Storage System permanently (or may be
+                transferred to another game, as long as the Pokémon is never
+                able to be used again during this run)
+              </p>
+            </li>
+            <li>
+              <p class="card-text">
+                The player may only catch the first wild Pokémon encountered in
+                each area, and none else. If the first wild Pokémon encountered
+                faints or flees, there are no second chances. If the first
+                encounter in the area is a double battle, the player is free to
+                choose which of the two wild Pokémon they would like to catch
+                but may only catch one of them. This restriction does not apply
+                to Pokémon able to be captured during static encounters, nor to
+                Shiny Pokémon
+              </p>
+            </li>
+          </ul>
+        </v-card-text>
+      </v-card>
+      <v-card class="info-card" id="secondary-rules">
+        <v-card-subtitle class="card-text"
+          >Other commonly used rules</v-card-subtitle
+        >
+        <v-card-text>
+          <ul>
+            <li>
+              <p class="card-text">
+                The player must nickname all of their Pokémon, for the sake of
+                forming stronger emotional bonds
+              </p>
+            </li>
+            <li>
+              <p class="card-text">
+                The "first wild Pokémon in each area" rule does not apply in an
+                area until a species or evolution line is encountered that has
+                not been caught yet
+              </p>
+            </li>
+          </ul>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -11,4 +78,31 @@ export default class Home extends Vue {}
 
 <style lang="scss" scoped>
 @import "@/styles/global.scss";
+
+.row {
+  width: 100%;
+  margin: 0;
+}
+
+.col {
+  height: 100%;
+}
+
+.v-card {
+  width: 100%;
+}
+
+#home-info,
+#rules {
+  overflow-y: auto;
+}
+
+#videos-card,
+#secondary-rules {
+  margin-top: 20px;
+}
+
+.card-text {
+  color: #999999;
+}
 </style>
